dbe0042f1e4791ef12809d6ea89adbeb32d09858
1evity <124527764+1evity@users.noreply.github.com>
2026-02-14 12:21:56 +0400

    Fix chat post fail

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatterController;
 
 Route::get('/', [ChatterController::class, 'index']);
-Route::get('/chats', [ChatterController::class, 'store']);
+Route::post('/chats', [ChatterController::class, 'store']);
